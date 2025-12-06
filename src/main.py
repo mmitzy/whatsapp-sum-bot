c575ae446814d57f1a28b61d3882342f01482683
Sibo523 <roisi20041@gmail.com>
2025-12-06 19:43:49 +0200

    feat(main,config,uv_generated_files): Removed unintended problems

--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
-from fastapi import FastAPI, Request
+from functools import lru_cache
+from fastapi import FastAPI, Request, Depends
+from typing import Annotated
 from fastapi.responses import PlainTextResponse, JSONResponse
+from .config import Settings
 
 app = FastAPI()
 
-# Make sure we match this token in meta
-VERIFY_TOKEN = "ben_is_a_cute_lemur"
+
+# VERIFY_TOKEN =  #should not be visible to everybody maybe using config would be better
+@lru_cache
+def get_settings():
+    return Settings()
+
 
 # Verify webhook endpoint
 @app.get("/webhook")
-async def verify_webhook(request: Request):
+def verify_webhook(
+    request: Request, settings: Annotated[Settings, Depends(get_settings)]
+):
     params = request.query_params
     mode = params.get("hub.mode")
     token = params.get("hub.verify_token")
@@ -19,10 +28,11 @@ async def verify_webhook(request: Request):
     print("token:", token)
     print("challenge:", challenge)
 
-    if mode == "subscribe" and token == VERIFY_TOKEN:
+    if mode == "subscribe" and token == settings.VERIFY_TOKEN:
         return PlainTextResponse(challenge)
 
     return PlainTextResponse("Verification failed", status_code=403)
+
 
 # Handle incoming webhook messages
 @app.post("/webhook")
